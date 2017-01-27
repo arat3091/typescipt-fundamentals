3f3367a1f8f8482d2ea7547a06d3762059936fc1
ankur.ratra <ankur.ratra@powerhouse.nl>
2017-01-27 14:53:47 +0100

    start

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+function main() {
+    var app = document.getElementById('app');
+    setInterval(function () {
+        app.innerHTML = generateRandomId();
+    }, 1000);
+}
+function generateRandomId() {
+    return '#' + Math.random().toString(36).substr(2, 7);
+}
